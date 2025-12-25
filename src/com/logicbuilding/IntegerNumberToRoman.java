9806cf6634ce542aa7b5389458add45577f99f71
Mitu <mitukumari2024@gmail.com>
2025-12-24 23:54:42 -0600

    Implement algorithm for integer to Roman numeral conversion

---
+++ src/com/logicbuilding/IntegerNumberToRoman.java
@@ -0,0 +1,33 @@
+package com.logicbuilding;
+
+public class IntegerNumberToRoman {
+
+	public static void main(String[] args) {
+		int num=23;
+		String symbols[]= {"L","X","V","I"};
+		int values[]= {50,10,5,1};
+		
+		StringBuilder sb=new StringBuilder();
+		
+		for(int i=0;i<values.length;i++) {
+			while(num>=values[i]) {
+				num=num-values[i];
+				sb.append(symbols[i]);
+				
+				
+			}
+			
+			
+			
+		}
+		
+		System.out.println(sb.toString());
+		
+		
+		
+		
+		
+
+	}
+
+}
